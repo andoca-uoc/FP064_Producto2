08aaaf3c58eb1a7d7683a0ff743637078e055d04
andoca-uoc <96543054+andoca-uoc@users.noreply.github.com>
2023-11-04 21:19:22 +0100

    Add files via upload

--- index.php
+++ index.php
@@ -15,28 +15,30 @@ $user->login();
     <title>EVENTS APP</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
   </head>
-  <body>
-    <div class = "container">
-        <div class = "container">
-            <h1>Introduce tus credenciales</h1>
-        <form class="col-md-12 well" action="controllers/login.php" method="POST">   
-        <!--Id_usuario-->
+<body>
+  <div class="display-9 text-center">
+    <div class="container">
+      <h1>Introduce tus credenciales</h1>
+    </div>
+      <form action="controllers/login.php" method="POST">
+      <!--Id_usuario-->
+      <div class="mb-3">
         <label for="Id_usuario">Id de usuario</label><br>
         <input type="id" name="Id_usuario" placeholder="Introduce tu id de usuario">
-        </div>
-        <!--Password-->
-        <div class = "col-md-12 well">
+      </div>
+      <!--Password-->
+      <div class="mb-3">
         <label for="Password">contraseña</label><br>
-        <input type="password" name="Password" placeholder="Introduce tu contraseña">   
-        </div>
-        <!--Botón de envío-->
+        <input type="password" name="Password" placeholder="Introduce tu contraseña">
+      </div>
+  <!--Botón de envío-->
         <input class="submit" type="submit" name="login_std" value="Entra">
-    </form>
-        <p>
-          <a href="views/signup_view.php">Registrarse</a>
-        </p>
-    </div>
-  </body>
+      </form>
+      <p>
+        <a href="views/signup_view.php">Registrarse</a>
+      </p>
+  </div>
+</body>
 </html>
 
 
