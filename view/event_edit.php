94ead5fde22e3ca514e3a8e3b0aec8d3a3f7d247
andoca-uoc <96543054+andoca-uoc@users.noreply.github.com>
2023-11-07 20:15:51 +0100

    Update event_edit.php

--- view/event_edit.php
+++ view/event_edit.php
@@ -1,94 +1,82 @@
 <!DOCTYPE html>
 <html lang="es">
 <head>
-  <meta charset="utf-8">
-  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
-  <title>Registro Usuarios</title>
+    <meta charset="utf-8">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+    <title>Modificar acto</title>
 </head>
 <body>
-<form action="/view/event/event" method="post">
-    <?php if(isset($event)):?>
-    <input type="hidden" name="data[Acto] [Id_acto]"
-     value="<?=$event->getId_acto()?>">
-    <?php endif;?>
+<form action="controller/event_edit_controller" method="post">
     <div class="container mt-4">
         <div class="row>">
             <div class="col">
-                <h2><?=(isset($event))?'Modificar':'Nuevo'?>Acto</h2>
+                <h2>Modificar acto</h2>
             </div>
             <div class="col text-end">
                 <a href="/view/event" role="button"
-                    class="btn btn-secondary">Cancelar</a>
-                <button type="submit" class="btn btn-primary">
-                    <?=(isset($event))?'Modificar':'Crear'?>
-                </button>
+                   class="btn btn-secondary">Cancelar</a>
+                <button type="submit" class="btn btn-primary">Crear</button>
             </div>
         </div>
 
         <div class="form-floating mb-3">
-            <input type="text" class="form-control"
-                id="floatingInput" placeholder="Id_acto"
-                name="data [Actos] [Id_acto]"
-                value="<?=(isset($event))?$event->getId_acto() : ''?>">
-            <label for="floatingInput">Id_acto</label>
+            <input type="hidden" id="floatingInput"
+                   name="data [Actos] [Id_acto]"
         </div>
 
         <div class="form-floating mb-3">
-            <input type="text" name="data [Actos] [Fecha] "
-            class="form-control"
-                id="floatingInput" placeholder="Fecha"
-                aria-describedby="fechaHelpBlock">
+            <input type="date" name="data [Actos] [Fecha] "
+                   class="form-control"
+                   id="floatingInput" placeholder="Fecha"
+                   aria-describedby="fechaHelpBlock">
             <label for="floatingInput">Fecha</label>
             <div id="fechaHelpBlock" class="form-text">
-            Fecha con formato YYY-MM-DD    
+                Fecha en formato YYYY-MM-DD (en caso de tener que cambiar date por text por Mysql)
             </div>
         </div>
 
         <div class="form-floating mb-3">
-            <input type="text" class="form-control"
-                id="floatingInput" placeholder="Hora"
-                name="data [Actos] [Hora] "
-                value="<?=(isset($event))?$event->getHora() : ''?>">
+            <input type="time" name="data [Actos] [Hora] "
+                   class="form-control"
+                   id="floatingInput" placeholder="Hora">
             <label for="floatingInput">Hora</label>
+            <div id="fechaHelpBlock" class="form-text">
+                Hora en formato HH:MM (en caso de tener que cambiar time por text por Mysql)
+            </div>
         </div>
 
         <div class="form-floating mb-3">
-            <input type="text" class="form-control"
-                id="floatingInput" placeholder="Titulo"
-                name="data [Actos] [Titulo] "
-                value="<?=(isset($event))?$event->getTitulo() : ''?>">
-            <label for="floatingInput">FTitulo</label>
+            <input type="text" name="data [Actos] [Titulo] "
+                   class="form-control"
+                   id="floatingInput" placeholder="Titulo">
+            <label for="floatingInput">Titulo</label>
         </div>
 
         <div class="form-floating mb-3">
-            <input type="text" class="form-control" 
-                id="floatingInput" placeholder="Descripcion_corta"
-                name="data [Actos] [Descripcion_corta] "
-                value="<?=(isset($event))?$event->getDescripcion_corta() : ''?>">
-            <label for="floatingInput">Descripcion_corta</label>
+            <input type="text" name="data [Actos] [Descripcion_corta] "
+                   class="form-control"
+                   id="floatingInput" placeholder="Descripcion_corta">
+            <label for="floatingInput">Descripcion corta</label>
         </div>
 
         <div class="form-floating mb-3">
-            <input type="text" class="form-control" 
-                id="floatingInput" placeholder="Descripcion_larga"
-                name="data [Actos] [Descripcion_larga] "
-                value="<?=(isset($event))?$event->getDescripcion_larga() : ''?>">
-            <label for="floatingInput">Descripcion_corta</label>
+            <input type="text" name="data [Actos] [Descripcion_larga] "
+                   class="form-control"
+                   id="floatingInput" placeholder="Descripcion_larga">
+            <label for="floatingInput">Descripcion larga</label>
         </div>
 
         <div class="form-floating mb-3">
-            <input type="text" class="form-control" 
-                id="floatingInput" placeholder="Num_asistentes"
-                name="data [Actos] [Num_asistentes] "
-                value="<?=(isset($event))?$event->getNum_asistentesa() : ''?>">
+            <input type="number" name="data [Actos] [Num_asistentes] "
+                   class="form-control"
+                   id="floatingInput" placeholder="Num_asistentes">
             <label for="floatingInput">Número de asistentes</label>
         </div>
 
         <div class="form-floating mb-3">
-            <input type="text" name="data [Actos] [Id_tipo_acto] "
-            class="form-control"
-                id="floatingInput" placeholder="Id_tipo_acto">
-            <label for="floatingInput">Id_tipo_acto</label>
+            <input type="hidden" id="floatingInput"
+                   name="data [Actos] [Id_tipo_acto]">
+
         </div>
     </div>
 </form>
