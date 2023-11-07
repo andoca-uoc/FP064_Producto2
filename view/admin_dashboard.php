ed72ba18dcd3547f4cc0f0ac10ea38dad4ede0c0
andoca-uoc <96543054+andoca-uoc@users.noreply.github.com>
2023-11-07 19:53:12 +0100

    Update admin_dashboard.php

--- view/admin_dashboard.php
+++ view/admin_dashboard.php
@@ -1,10 +1,3 @@
-<?php
-
-//para que no dé fallo hasta que esté bien construido model y controller
-$events = array();
-
-?>
-
 <!DOCTYPE html>
 <html>
 <head>
@@ -13,7 +6,7 @@ $events = array();
   <title>Panel Admin + Sidebar</title>
 
   <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
-  <link rel="stylesheet" href="/style.css">
+  <link rel="stylesheet" href="style.css">
 </head>
 <body>
 <div class="wrapper">
@@ -25,7 +18,7 @@ $events = array();
 
     <ul class="list-unstyled components">
       <li>
-        <a href="/view/event.php">Actos</a>
+        <a href="/event_view.php">Actos</a>
       </li>
       <li>
         <a href="/views/speaker.php">Ponentes</a>
@@ -44,7 +37,7 @@ $events = array();
       </ul>
 
     <ul class="list-unstyled CTAs">
-      <li><a href="/view/login.php" class="article">Desconectarse</a></li>
+      <li><a href="/login.php" class="article">Desconectarse</a></li>
     </ul>
   </nav>
 
@@ -55,57 +48,10 @@ $events = array();
         <h1>Gestión de Actos</h1>
       </div>
     </nav>
-        <div class="container mt-4">
-            <div class="row">
-                <div class="col text-end">
-                    <a href="/view/event_create.php" role="button" class="btn btn-primary">Nuevo acto</a>
-                </div>
-            </div>
-            <div class="row">
-                <table class="table table-light table-striped table-hover">
-                    <thead>
-                    <tr>
-                        <th scope="col">Id_acto</th>
-                        <th scope="col">Fecha</th>
-                        <th scope="col">Hora</th>
-                        <th scope="col">Titulo</th>
-                        <th scope="col">Descripción_corta</th>
-                        <th scope="col">Descripcion_larga</th>
-                        <th scope="col">Num_asistentes</th>
-                        <th scope="col">Id_tipo_acto</th>
-                        <th scope="col">Operaciones</th>
-                    </tr>
-                    </thead>
-                    <tbody>
-                    <?php foreach($events as $event) :?>
-                        <tr>
-                            <th scope="row"><?=$event->getId_acto()?></th>
-                            <td><?=$event->getFecha()?></td>
-                            <td><?=$event->getHora()?></td>
-                            <td><?=$event->getTitulo()?></td>
-                            <td><?=$event->getDescripcion_corta()?></td>
-                            <td><?=$event->getDescripcion_larga()?></td>
-                            <td><?=$event->getNum_asistencia()?></td>
-                            <td><?=$event->getId_tipo_acto()?></td>
-                            <td>
-                                <div class="btn-group" role="group">
-                                    <a href="/view/event_edit.php" role="button"
-                                       class="btn btn-sm btn-outline-secondary">
-                                        <i class="bi bi-pencil"></i>
-                                    </a>
-                                    <a href="#" role="button"
-                                       class="btn btn-sm btn-outline-danger">
-                                        <i class="bi bi-trash"></i>
-                                    </a>
-                                </div>
-                            </td>
-                        </tr>
-                    <?php endforeach; ?>
-                    </tbody>
-                </table>
-            </div>
-        </div>
-    </div>
+
+      //
+      <p>Aquí va el contenido</p>
+
   </div>
 </div>
 </body>
