658dfbb2f6196fcadb7cfb1f6f914bcf1b820fc7
andoca-uoc <96543054+andoca-uoc@users.noreply.github.com>
2023-11-12 10:25:27 +0100

    Update borrar.php

--- app/controllers/actos/borrar.php
+++ app/controllers/actos/borrar.php
@@ -1,17 +1,25 @@
 <?php
-include '../models/actos.php';
+//Encabezados (HEADERS)
+header('Access-Control-Allow-Origin: *');
+header('Content-Type: application/json');
+header('Access-Control-Allow-Methods: DELETE');
+header('Access-Control-Allow-Headers: Access-Control-Allow-Headers, Content-Type, Access-Control-Allow-Methods, Authorization, X-Requested-With');
 
-    //Instancionamos la base de datos y conexión
-    $baseDatos = new Basemysql();
+include_once '../../config/Dbmysql.php';
+include_once '../../models/actos.php';
+
+
+//Instancionamos la base de datos y conexión
+    $baseDatos = new Dbmysql();
     $db = $baseDatos->connect();
 
     //Instanciamos el objeto categoría
-    $acto = new actos($db);
+    $acto = new \app\models\Acto($db);
 
     $data = json_decode(file_get_contents("php://input"));
 
-    //Setear el id de categoría
-    $acto->id = $data->id;
+    //Setear el id de acto
+    $acto->Id_acto = $data->Id_acto;
 
     //Crear categorías
     if($acto->borrar()){
