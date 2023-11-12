bb7701a9885444d7d2c8633cd52890780ce17812
andoca-uoc <96543054+andoca-uoc@users.noreply.github.com>
2023-11-12 10:30:16 +0100

    Update acto_editar.php

--- app/views/acto_editar.php
+++ app/views/acto_editar.php
@@ -1,72 +1,84 @@
+<?php include '../includes/header.php'; ?>
 
-<!DOCTYPE html>
-<html lang="es">
-<head>
-    <meta charset="utf-8">
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
-    <title></title>
-</head>
-<body>
-<div class="container mt-4">
-    <div class="row">
-        <div class="col text-end">
-            <a href="event_create.php" role="button" class="btn btn-primary">Nuevo acto</a>
+<div class="row">
+    <div class="col-sm-12">
+        <?php if(isset($error)) : ?>
+            <div class="alert alert-danger alert-dismissible fade show" role="alert">
+                <strong><?php echo $error; ?></strong>
+                <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
+            </div>
+        <?php endif; ?>
+    </div>
+
+
+
+    <form action="acto_editar.php" method="POST">
+        <div class="container mt-4">
+            <div class="row>">
+                <div class="col">
+                    <h2>Modificar acto</h2>
+                </div>
+                <div class="col text-end">
+                    <a href="" role="button"
+                       class="btn btn-secondary">Cancelar</a>
+                    <button type="submit" class="btn btn-primary">Modificar</button>
+                </div>
+            </div>
+            <form action="" method="POST" enctype="multipart/form-data">
+            <div class="form-floating mb-3">
+                <input type="hidden" id="floatingInput" name="Id acto" value="<?php echo $resultado->Id_acto; ?>">
+            </div>
+
+            <div class="form-floating mb-3">
+                <input type="text" class="form-control" id="floatingInput"
+                       name="Titulo" placeholder="Titulo" value="<?php echo $resultado->Titulo; ?>">
+                <label for="floatingInput">Titulo</label>
+            </div>
+
+            <div class="form-floating mb-3">
+                <input type="text" class="form-control" id="floatingInput"
+                       name="Fecha" placeholder="Fecha"  aria-describedby="fechaHelpBlock" value="<?php echo $resultado->Fecha; ?>">
+                <label for="floatingInput">Fecha</label>
+                <div class="form-text">
+                    Fecha con formato año-mes-día YYYY-MM-DD
+                </div>
+            </div>
+
+            <div class="form-floating mb-3">
+                <input type="text" class="form-control" id="floatingInput"
+                       name="Hora" placeholder="Hora" value="<?php echo $resultado->Hora; ?>">
+                <label for="floatingInput">Hora</label>
+                <div class="form-text">
+                    Hora con formato hora:minutos:segundos hh:mm:ss
+                </div>
+            </div>
+
+
+            <div class="form-floating mb-3">
+                <input type="text" class="form-control" id="floatingInput"
+                       name="Descripcion_corta" placeholder="Descripcion_corta" value="<?php echo $resultado->Descripcion_corta; ?>">
+                <label for="floatingInput">Descripción corta</label>
+            </div>
+
+            <div class="form-floating mb-3">
+                <input type="text" class="form-control" id="floatingInput"
+                       name="Descripcion_larga" placeholder="Descripcion_larga" value="<?php echo $resultado->Descripcion_larga; ?>">
+                <label for="floatingInput">Descripción larga</label>
+            </div>
+
+            <div class="form-floating mb-3">
+                <input type="number" class="form-control" id="floatingInput"
+                       name="Num_asistentes" placeholder="Num_asistentes" value="<?php echo $resultado->Num_asistentes; ?>">
+                <label for="floatingInput">Número de asistentes</label>
+            </div>
+
+            <div class="form-floating mb-3">
+                <input type="number" id="floatingInput"
+                       name="Id_tipo_acto" value="<?php echo $resultado->Id_tipo_acto; ?>">
+            </div>
         </div>
-    </div>
-    <div class="row">
-        <table class="table table-light table-striped table-hover">
-            <thead>
-            <tr>
-                <th scope="col">Id_acto</th>
-                <th scope="col">Fecha</th>
-                <th scope="col">Hora</th>
-                <th scope="col">Titulo</th>
-                <th scope="col">Descripción_corta</th>
-                <th scope="col">Descripcion_larga</th>
-                <th scope="col">Num_asistentes</th>
-                <th scope="col">Id_tipo_acto</th>
-                <th scope="col">Acciones</th>
-            </tr>
-            </thead>
-            <tbody>
-            <?php if ($rows): ?>
-            <?php foreach($rows as $row) {
+    </form>
+    </body>
+    </html>
 
-            }<tr>
-            <td>"><?= $date["Id_acto"]?></td>
-            <td>"><?= $date["Fecha"]?></td>
-            <td>"><?= $date["Hora"]?></td>
-            <td>"><?= $date["Titulo"]?></td>
-            <td>"><?= $date["Descripcion_corta"]?></td>
-            <td>"><?= $date["Descripcion_larga"]?></td>
-            <td>"><?= $date["Num_asistentes"]?></td>
-            <td>"><?= $date["Id_tipo_acto"]?></td>
 
-            }
-            }
-            else{
-            echo 'No hay resultados';
-            }
-            ?>
-            <div class="btn-group" role="group">
-                <a role="button" class="btn btn-sm btn-outline-secondary" href="\controller/event_modify.php?id=".$result -> id."\">Modificar</a>
-            </div>
-            <div class="btn-group" role="group">
-                <a role="button" class="btn btn-sm btn-outline-danger" href="\controller/event_delete.php?id=".$result -> id."\">Borrar</a>
-
-                <button class="btn btn-sm btn-outline-danger" type="button"
-                        onclick="fetch('/view/event/<?=$event->getId_actos()?>'
-                            {method: 'DELETE'})
-                            .then(data => location.reload() )"><i class="bi bi-trash"></i>
-                </button>
-            </div>
-            </td>
-            </tr>
-            </tbody>
-        </table>
-    </div>
-</div>
-</div>
-</div>
-</body>
-</html>
