a7498f0303c9d5b730ce53adaa541f7f8bb6f4b5
andoca-uoc <96543054+andoca-uoc@users.noreply.github.com>
2023-11-12 10:27:18 +0100

    Update header.php

--- app/includes/header.php
+++ app/includes/header.php
@@ -1,14 +1,42 @@
-<?php include '../config/config.php'?>
-<?php include '../config/Basemysql.php'?>
-<?php include '../models/actos.php'?>
-
 <!DOCTYPE html>
-<html lang="es">
-    <head>
-        <meta charset="utf-8">
-        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet">
-        <link rel="stylesheet" href="../assets/css/admin.css">
-        <title>Panel Admin</title>
-    </head>
+<html>
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Panel Admin + Sidebar</title>
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+    <link rel="stylesheet" href="../assets/css/admin.css">
+</head>
+<body>
+<div class="wrapper">
+    <!--Sidebar Izq-->
+    <nav id="sidebar">
+        <div class="sidebar-header">
+            <h3>Panel de Administración</h3>
+        </div>
 
+        <ul class="list-unstyled components">
+            <li>
+                <a href="#">Actos</a>
+            </li>
+            <li>
+                <a href="#">Ponentes</a>
+            </li>
+            <li>
+                <a href="#">Tipos de actos</a>
+            </li>
+            <li>
+                <a href="#">Inscritos</a>
+            </li>
+        </ul>
+        <ul class="list-unstyled CTAs">
+            <li  <li class="list-group-item list-group-item-info">
+                <a href="#">Perfil</a>
+            </li>
+        </ul>
+
+        <ul class="list-unstyled CTAs">
+            <li><a href="#" class="article">Desconectarse</a></li>
+        </ul>
+    </nav>
 
