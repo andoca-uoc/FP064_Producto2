ce9c53490413bdc0584cc7bfb1837a8c84a31f62
andoca-uoc <96543054+andoca-uoc@users.noreply.github.com>
2023-11-13 11:59:29 +0100

    Update crear.php

--- app/controllers/actos/crear.php
+++ app/controllers/actos/crear.php
@@ -6,26 +6,25 @@
 	if(!$db) {
 		die("Error al conectar con la base de datos");
 	}
-
-
-	//if ($_SERVER['REQUEST_METHOD'] === 'POST')
-	if (isset($_POST['crearActo']))
+	
+	if ($_SERVER['REQUEST_METHOD'] === 'POST')
 	{
-		//Se obtienen los valores
-		$Fecha = $_POST['Fecha'];
-		$Hora = $_POST['Hora'];
-		$Titulo = $_POST['Titulo'];
-		$Descripcion_corta = $_POST['Descripcion_corta'];
-		$Descripcion_larga = $_POST['Descripcion_larga'];
-		$Num_asistentes = $_POST['Num_asistentes'];
-		$Id_tipo_acto = $_POST['Id_tipo_acto'];
-
 		//Se instancia el objeto
 		$acto = new Acto();
-
+		//Se obtienen los valores
+		//$acto->Id_acto = $_POST['id'];
+		$acto->Fecha = $_POST['Fecha'];
+		$acto->Hora = $_POST['Hora'];
+		$acto->Titulo = $_POST['Titulo'];
+		$acto->Descripcion_corta = $_POST['Descripcion_corta'];
+		$acto->Descripcion_larga = $_POST['Descripcion_larga'];
+		$acto->Num_asistentes = $_POST['Num_asistentes'];
+		$acto->Id_tipo_acto = $_POST['Id_tipo_acto'];
+		
 		if ($acto->crear()) {
-			$mensaje = "Acto creado correctamente";
-			header("Location: acto.php");
-			}
+			header('Location: /views/admin_panel.php');
+		} else {
+			echo "Error al crear el acto";
+		}
 	}
 ?>
