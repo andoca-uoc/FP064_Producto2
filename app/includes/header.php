e275b4392dbe7ed5c8cf685ce05002c5c9e62738
andoca-uoc <96543054+andoca-uoc@users.noreply.github.com>
2023-11-13 19:46:13 +0100

    Update header.php

--- app/includes/header.php
+++ app/includes/header.php
@@ -5,39 +5,62 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Panel Admin + Sidebar</title>
 	<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet">
-    <link rel="stylesheet" href="/assets/css/admin.css">
-
+    <link rel="stylesheet" href="/assets/css/general.css">
 </head>
 <body>
-<div class="wrapper">
-    <!--Sidebar Izq-->
-    <nav id="sidebar">
-        <div class="sidebar-header">
-            <h3>Panel de Administración</h3>
-        </div>
+<!--side izq-->
+div class="container-fluid">
+<div class="row flex-nowrap">
+	<div class="col-auto col-md-3 col-xl-2 px-sm-2 px-0 bg-dark">
+		<div class="d-flex flex-column align-items-center align-items-sm-start px-3 pt-2 text-white min-vh-100">
+			<a href="/" class="d-flex align-items-center pb-3 mb-md-0 me-md-auto text-white text-decoration-none">
+				<span class="fs-5 d-none d-sm-inline">Admin Panel</span>
+			</a>
+			<ul class="nav nav-pills flex-column mb-sm-auto mb-0 align-items-center align-items-sm-start" id="menu">
+				<li class="nav-item">
+					<a href="#" class="nav-link align-middle px-0">
+						<i class="fs-4 bi-house"></i> <span class="ms-1 d-none d-sm-inline">Home</span>
+					</a>
+				</li>
+				<li>
+					<a href="#submenu1" data-bs-toggle="collapse" class="nav-link px-0 align-middle">
+						<i class="fs-4 bi-speedometer2"></i> <span class="ms-1 d-none d-sm-inline">Eventos</span> </a>
+					<ul class="collapse show nav flex-column ms-1" id="submenu1" data-bs-parent="#menu">
+						<li class="w-100">
+							<a href="#" class="nav-link px-0"> <span class="d-none d-sm-inline">Tipos</span> Eventos </a>
+						</li>
 
-        <ul class="list-unstyled components">
-            <li>
-                <a href="/views/admin_panel.php">Actos</a>
-            </li>
-            <li>
-                <a href="/views/ponente_panel.php">Ponentes</a>
-            </li>
-            <li>
-                <a href="/views/tipo_acto_panel">Tipos de actos</a>
-            </li>
-            <li>
-                <a href="#">Inscritos</a>
-            </li>
-        </ul>
-        <ul class="list-unstyled CTAs">
-            <li  <li class="list-group-item list-group-item-info">
-                <a href="#">Perfil</a>
-            </li>
-        </ul>
+					</ul>
+				</li>
+				<li>
+					<a href="#" class="nav-link px-0 align-middle">
+						<i class="fs-4 bi-table"></i> <span class="ms-1 d-none d-sm-inline">Ponentes</span></a>
+				</li>
 
-        <ul class="list-unstyled CTAs">
-            <li><a href="#" class="article">Desconectarse</a></li>
-        </ul>
-    </nav>
+				<li>
+					<a href="#submenu3" data-bs-toggle="collapse" class="nav-link px-0 align-middle">
+						<i class="fs-4 bi-grid"></i> <span class="ms-1 d-none d-sm-inline">Inscritos</span> </a>
+
+				</li>
+				<li>
+					<a href="#" class="nav-link px-0 align-middle">
+						<i class="fs-4 bi-people"></i> <span class="ms-1 d-none d-sm-inline"></span> </a>
+				</li>
+			</ul>
+			<hr>
+			<div class="dropdown pb-4">
+				<a href="#" class="d-flex align-items-center text-white text-decoration-none dropdown-toggle" id="dropdownUser1" data-bs-toggle="dropdown" aria-expanded="false">
+					<img src="#" alt="" width="30" height="30" class="rounded-circle">
+					<span class="d-none d-sm-inline mx-1">Perfil</span>
+				</a>
+				<ul class="dropdown-menu dropdown-menu-dark text-small shadow" aria-labelledby="dropdownUser1">
+					<li><a class="dropdown-item" href="#">Modificar</a></li>
+					<li>
+						<hr class="dropdown-divider">
+					</li>
+					<li><a class="dropdown-item" href="#">Desconectar</a></li>
+				</ul>
+			</div>
+		</div>
+	</div>
 
