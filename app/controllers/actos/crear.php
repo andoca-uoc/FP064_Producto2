b6b306ce25047c347a3d8717756d118628187e7b
andoca-uoc <96543054+andoca-uoc@users.noreply.github.com>
2023-11-13 01:10:48 +0100

    Update crear.php

--- app/controllers/actos/crear.php
+++ app/controllers/actos/crear.php
@@ -1,34 +1,31 @@
 <?php
-    require_once __DIR__ . '/../models/db.php';
-    require_once __DIR__ . '/../models/actos.php';
+    require_once __DIR__ . '/../../models/db.php';
+    require_once __DIR__ . '/../../models/actos.php';
+
+	$db = db_connect();
+	if(!$db) {
+		die("Error al conectar con la base de datos");
+	}
 
 
-    if ($_SERVER['REQUEST_METHOD'] === 'POST') {
-        $db = db_connect();
+	//if ($_SERVER['REQUEST_METHOD'] === 'POST')
+	if (isset($_POST['crearActo']))
+	{
+		//Se obtienen los valores
+		$Fecha = $_POST['Fecha'];
+		$Hora = $_POST['Hora'];
+		$Titulo = $_POST['Titulo'];
+		$Descripcion_corta = $_POST['Descripcion_corta'];
+		$Descripcion_larga = $_POST['Descripcion_larga'];
+		$Num_asistentes = $_POST['Num_asistentes'];
+		$Id_tipo_acto = $_POST['Id_tipo_acto'];
 
-        if (!$db) {
-            die("Error al conectar con la base de datos");
-        }
+		//Se instancia el objeto
+		$acto = new Acto();
 
-
-        $acto = new Acto($db);
-
-
-        $acto->Fecha = $_POST['Fecha'] ?? null;
-        $acto->Hora = $_POST['Hora'] ?? null;
-        $acto->Titulo = $_POST['Titulo'] ?? null;
-        $acto->Descripcion_corta = $_POST['Descripcion_corta'] ?? null;
-        $acto->Descripcion_larga = $_POST['Descripcion_larga'] ?? null;
-        $acto->Num_asistentes = $_POST['Num_asistentes'] ?? null;
-        $acto->Id_tipo_acto = $_POST['Id_tipo_acto'] ?? null;
-
-
-        if ($acto->crear()) {
-            header("Location: /ruta/a/tu/vista/exito.php");
-        } else {
-            header("Location: /ruta/a/tu/vista/error.php");
-        }
-    } else {
-        header("Location: /ruta/a/tu/formulario.php");
-    }
+		if ($acto->crear()) {
+			$mensaje = "Acto creado correctamente";
+			header("Location: acto.php");
+			}
+	}
 ?>
