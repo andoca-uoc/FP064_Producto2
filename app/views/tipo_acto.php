2df960fa6270cf2e39365f8539ffe270ef017b4f
andoca-uoc <96543054+andoca-uoc@users.noreply.github.com>
2023-11-13 19:43:56 +0100

    Update tipo_acto.php

--- app/views/tipo_acto.php
+++ app/views/tipo_acto.php
@@ -5,7 +5,9 @@
 <head>
 	<meta charset="utf-8">
 	<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet">
-	<link rel="stylesheet" href="../assets/css/general.css">
+	<link rel="stylesheet" href="/assets/css/admin.css">
+	<link rel="stylesheet" href="/assets/css/general.css">
+
 	<title>Panel Admin</title>
 </head>
 
