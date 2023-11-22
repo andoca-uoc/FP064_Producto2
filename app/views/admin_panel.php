dd98e770c4943b6cc1eada2bdacbdb862a26ee92
3ntr0pia <74710813+3ntr0pia@users.noreply.github.com>
2023-11-22 10:41:45 +0100

    quitado modales

--- app/views/admin_panel.php
+++ app/views/admin_panel.php
@@ -59,12 +59,12 @@ include './modales.php';
             </ul>
         </nav>
 
-        <div id="content">
+        <!-- <div id="content">
             <?php
             $page = $_GET['page'] ?? 'default';
             handleRoute($page);
             ?>
-        </div>
+        </div> -->
     </div>
 
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js"></script>
